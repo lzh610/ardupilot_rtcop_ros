05ddcf23488a683681ba4f77fa609020901da18d
lzh610 <liuzihan19960610@yahoo.co.jp>
2021-02-12 17:13:25 +0900

    LCNs

---
+++ LCN/test_13_node.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include "Generated/API.h"
+#include "Generated/BaseLayer.h"
+#include "gnc_functions.h"
+#include "ActiveController.h"
+#include "string.h"
+#include "ardupilot_rtcop_ros/activation_msg.h"
+
+using namespace gnc;
+using namespace PLAM;
+
+bool msg_return(ardupilot_rtcop_ros::activation_msg::Request &req,
+         ardupilot_rtcop_ros::activation_msg::Response &res)
+{
+    // テスト版として応答メッセージに"_ok"を追加だけ
+    if(strcmp(req.activation.c_str(),"test_13_activate") == 0 || strcmp(req.activation.c_str(),"test_13_deactivate") == 0)
+        res.activation_return = req.activation + "_ok";
+    ROS_INFO("request: %s", res.activation_return.c_str()); 
+    return true;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc,argv,"test_13_node");
+    ros::NodeHandle test_13_node;
+    ros::ServiceServer service = test_13_node.advertiseService("test_13_activation", msg_return);
+    ros::spin();
+    return 0;
+}
